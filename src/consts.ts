1d9d32256e2e8a728dab588aa909fe605eb257b8
Daniel Christo <daniel.c@thinkific.com>
2024-06-09 21:44:35 -0400

    refactor(website): change website name and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Daniel\'s Website';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Daniel Christo';
+export const SITE_DESCRIPTION = 'Daniel Christo\'s CV and Website';
 import { Daniel } from './models/daniel.model';
 
 
