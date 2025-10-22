e491e5c4924a1b12634e31a5ad0bcd002608e7a2
Daniel Christo <daniel.c@thinkific.com>
2025-10-22 16:17:22 -0400

    fix(website): don't publish non-dated blog posts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,11 +4,13 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+	// Filter out posts without pubDate since RSS requires publication dates
+	const postsWithDates = posts.filter(post => post.data.pubDate);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: postsWithDates.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
 		})),
